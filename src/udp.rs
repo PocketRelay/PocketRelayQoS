875c9157b64f48932c96aff40eba0f10f00e90e4
Jacobtread <jacobtread@gmail.com>
2023-10-09 12:07:01 +1300

    Split of udp packet handling into seperate tasks

--- src/udp.rs
+++ src/udp.rs
@@ -10,6 +10,7 @@ use crate::{constants::QOS_PORT, service::QService};
 
 #[derive(Debug, Clone)]
 pub struct QosHeader {
+    // 0002, 0003, 0005,
     pub u1: u32,
     pub request_id: u32,
     pub request_secret: u32,
@@ -19,7 +20,7 @@ pub struct QosHeader {
 
 impl QosHeader {
     pub fn from_slice(header: &[u8]) -> QosHeader {
-        let u1 = u32_from_slice(&header[0..4]); // 0002, 0003, 0005
+        let u1 = u32_from_slice(&header[0..4]);
         let request_id = u32_from_slice(&header[4..8]);
         let request_secret = u32_from_slice(&header[8..12]);
         let probe_number = u32_from_slice(&header[12..16]);
@@ -43,6 +44,7 @@ impl QosHeader {
     }
 }
 
+#[derive(Debug)]
 pub struct QosResponseV1 {
     pub header: QosHeader,
     pub ip: Ipv4Addr,
@@ -58,6 +60,7 @@ impl QosResponseV1 {
     }
 }
 
+#[derive(Debug)]
 pub struct QosResponseV2 {
     pub header: QosHeader,
     pub ubps: u32,
@@ -80,80 +83,18 @@ pub async fn start_server(service: Arc<QService>) {
         .await
         .unwrap();
 
-    // Buffer for the packet header
+    let socket = Arc::new(socket);
+
+    // Buffer for reciving messages
     let mut buffer = [0u8; 5048];
 
     loop {
         // Read bytes from the socket
         let (length, addr) = socket.recv_from(&mut buffer).await.unwrap();
-        if length < 20 {
-            error!("Client didn't send a message long enough to be a header");
-            continue;
-        }
 
-        let mut header = QosHeader::from_slice(&buffer[0..20]);
-
-        debug!("Query: {:?}", &header,);
-
-        // let addr_fake = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(104, 28, 193, 125), 38078));
-        let addr_fake = addr;
-
-        if header.request_id == 1 && header.request_secret == 0 {
-            debug!("Query type 1 responding with address to: {}", addr);
-
-            let SocketAddr::V4(socket_addr) = addr_fake else {
-                continue;
-            };
-
-            let response = QosResponseV1 {
-                header,
-                ip: *socket_addr.ip(),
-                port: socket_addr.port(),
-            };
-
-            let mut out = Vec::new();
-            response.write(&mut out);
-
-            // debug!("Message: {:?}", output);
-
-            debug!("SEND");
-
-            if let Err(err) = socket.send_to(&out, addr).await {
-                // TODO: Handle server unable to reach
-                error!("Unable to return message to client {}: {}", addr, err);
-            }
-        } else {
-            let request = service
-                .get_request_data(header.request_id, header.request_secret)
-                .await
-                .expect("Missing request data for request");
-
-            header.u2 = header.u2.swap_bytes();
-
-            if request.q_type == 2 {
-                debug!("Query type 2 responding with port and padding to: {}", addr);
-
-                let payload = &buffer[20..(length - 26)];
-
-                let response = QosResponseV2 {
-                    header,
-                    ubps: u32::from_be_bytes([0x00, 0x5b, 0x8d, 0x80]),
-                    port: request.client_port,
-                    payload: payload.to_vec(),
-                };
-
-                let mut out = Vec::new();
-                response.write(&mut out);
-
-                // debug!("Message: {:?}", output);
-                debug!("SEND");
-
-                if let Err(err) = socket.send_to(&out, addr).await {
-                    // TODO: Handle server unable to reach
-                    error!("Unable to return message to client {}: {}", addr, err);
-                }
-            }
-        }
+        // Copy the request bytes from the buffer
+        let buffer: Box<[u8]> = Box::from(&buffer[..length]);
+        tokio::spawn(handle(service.clone(), socket.clone(), addr, buffer));
     }
 }
 
@@ -161,4 +102,76 @@ fn u32_from_slice(slice: &[u8]) -> u32 {
     let mut a = [0u8; 4];
     a.copy_from_slice(slice);
     u32::from_be_bytes(a)
+}
+
+/// Handles a new udp request
+///
+/// # Arguments
+/// * socket - The udp socket bound for sending the response
+/// * addr - The address of the message sender
+/// * buffer - The received message buffer
+async fn handle(
+    service: Arc<QService>,
+    socket: Arc<UdpSocket>,
+    addr: SocketAddr,
+    buffer: Box<[u8]>,
+) {
+    if buffer.len() < 20 {
+        error!("Client didn't send a message long enough to be a header");
+        return;
+    }
+
+    let addr = match addr {
+        SocketAddr::V4(addr) => addr,
+        SocketAddr::V6(_) => {
+            error!("Got request from IPv6 address, don't know how to respond");
+            return;
+        }
+    };
+
+    let mut header = QosHeader::from_slice(&buffer[0..20]);
+    debug!("RECV: {:?}", &header);
+
+    let mut out: Vec<u8> = Vec::new();
+
+    if header.request_id == 1 && header.request_secret == 0 {
+        let response = QosResponseV1 {
+            header,
+            ip: *addr.ip(),
+            port: addr.port(),
+        };
+
+        debug!("SEND: {:?}", &response);
+
+        response.write(&mut out);
+    } else {
+        let request = service
+            .get_request_data(header.request_id, header.request_secret)
+            .await
+            .expect("Missing request data for request");
+
+        if request.q_type != 2 {
+            error!("Unepxected qos request");
+            return;
+        }
+
+        header.u2 = header.u2.swap_bytes();
+
+        let payload = &buffer[20..(buffer.len() - 26)];
+        let response = QosResponseV2 {
+            header,
+            ubps: u32::from_be_bytes([0x00, 0x5b, 0x8d, 0x80]),
+            port: 38078,
+            payload: payload.to_vec(),
+        };
+
+        debug!("SEND: {:?}", &response);
+
+        response.write(&mut out);
+    }
+
+    if let Err(err) = socket.send_to(&out, addr).await {
+        // TODO: Handle server unable to reach
+        error!("Unable to return message to client {}: {}", addr, err);
+    }
 }
